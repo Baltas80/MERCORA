046c5b7aaa70e42efb74670137c27a059e633319
Baltas80 <44073150+Baltas80@users.noreply.github.com>
2026-09-22 01:35:50 +0200

    Force public text through versioned content API

--- server/admin-management.js
+++ server/admin-management.js
@@ -19,8 +19,7 @@ const REPORT_STATUSES = new Set(['open','reviewing','resolved','dismissed']);
 const MONEY_TYPES = new Set(['percent','fixed']);
 const DISCOUNT_TARGETS = new Set(['global','store','listing','category']);
 const SITE_KEYS = new Set([
-  'site_name','site_mode','announcement','maintenance_message',
-  'new_listings_enabled','seller_registration_enabled','footer_notice','hero_title','hero_copy','buy_cta','sell_cta'
+  'site_mode','new_listings_enabled','seller_registration_enabled'
 ]);
 const SITE_MODES = new Set(['public','maintenance','restricted']);
 const SITE_BOOLEAN_KEYS = new Set(['new_listings_enabled','seller_registration_enabled']);
